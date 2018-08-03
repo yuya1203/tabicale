9e8fbc4384f4a73e0343c3e9a616feb33b19d993
ohashi0904 <ohashi904@yahoo.co.jp>
2018-08-03 14:56:28 +0900

    08/03テスト用に暫定的に値を入れてあります。

--- sql/tabicale.sql
+++ sql/tabicale.sql
@@ -21,34 +21,34 @@ logined tinyint not null default 0 comment "ログインフラグ",
 regist_date datetime not null comment "登録日",
 update_date datetime not null comment "更新日"
 )
-default charaset=utf8
+default charset=utf8
 comment="会員情報テーブル"
 ;
 insert into user_info values
 (1,"guest","guest","インターノウス","ゲストユーザー","いんたーのうす","げすとゆーざー",0,"guest@gmail.com",0,0,now(),now()),
-(2,"guest","guest2","インターノウス","ゲストユーザー2","いんたーのうす","げすとゆーざー2",0,"guest2@gmail.com",0,0,now(),now()),
-(3,"guest","guest3","インターノウス","ゲストユーザー3","いんたーのうす","げすとゆーざー3",0,"guest3@gmail.com",0,0,now(),now()),
-(4,"guest","guest4","インターノウス","ゲストユーザー4","いんたーのうす","げすとゆーざー4",0,"guest4@gmail.com",0,0,now(),now()),
-(5,"guest","guest5","インターノウス","ゲストユーザー5","いんたーのうす","げすとゆーざー5",0,"guest5@gmail.com",0,0,now(),now()),
-(6,"guest","guest6","インターノウス","ゲストユーザー6","いんたーのうす","げすとゆーざー6",0,"guest6@gmail.com",0,0,now(),now()),
-(7,"guest","guest7","インターノウス","ゲストユーザー7","いんたーのうす","げすとゆーざー7",0,"guest7@gmail.com",0,0,now(),now()),
-(8,"guest","guest8","インターノウス","ゲストユーザー8","いんたーのうす","げすとゆーざー8",0,"guest8@gmail.com",0,0,now(),now()),
-(9,"guest","guest9","インターノウス","ゲストユーザー9","いんたーのうす","げすとゆーざー9",0,"guest9@gmail.com",0,0,now(),now()),
-(10,"guest","guest10","インターノウス","ゲストユーザー10","いんたーのうす","げすとゆーざー10",0,"guest10@gmail.com",0,0,now(),now()),
-(11,"guest","guest11","インターノウス","ゲストユーザー11","いんたーのうす","げすとゆーざー11",0,"guest11@gmail.com",0,0,now(),now()),
-(12,"guest","guest12","インターノウス","ゲストユーザー12","いんたーのうす","げすとゆーざー12",0,"guest12@gmail.com",0,0,now(),now()),
+(2,"guest2","guest2","インターノウス","ゲストユーザー2","いんたーのうす","げすとゆーざー2",0,"guest2@gmail.com",0,0,now(),now()),
+(3,"guest3","guest3","インターノウス","ゲストユーザー3","いんたーのうす","げすとゆーざー3",0,"guest3@gmail.com",0,0,now(),now()),
+(4,"guest4","guest4","インターノウス","ゲストユーザー4","いんたーのうす","げすとゆーざー4",0,"guest4@gmail.com",0,0,now(),now()),
+(5,"guest5","guest5","インターノウス","ゲストユーザー5","いんたーのうす","げすとゆーざー5",0,"guest5@gmail.com",0,0,now(),now()),
+(6,"guest6","guest6","インターノウス","ゲストユーザー6","いんたーのうす","げすとゆーざー6",0,"guest6@gmail.com",0,0,now(),now()),
+(7,"guest7","guest7","インターノウス","ゲストユーザー7","いんたーのうす","げすとゆーざー7",0,"guest7@gmail.com",0,0,now(),now()),
+(8,"guest8","guest8","インターノウス","ゲストユーザー8","いんたーのうす","げすとゆーざー8",0,"guest8@gmail.com",0,0,now(),now()),
+(9,"guest9","guest9","インターノウス","ゲストユーザー9","いんたーのうす","げすとゆーざー9",0,"guest9@gmail.com",0,0,now(),now()),
+(10,"guest10","guest10","インターノウス","ゲストユーザー10","いんたーのうす","げすとゆーざー10",0,"guest10@gmail.com",0,0,now(),now()),
+(11,"guest11","guest11","インターノウス","ゲストユーザー11","いんたーのうす","げすとゆーざー11",0,"guest11@gmail.com",0,0,now(),now()),
+(12,"guest12","guest12","インターノウス","ゲストユーザー12","いんたーのうす","げすとゆーざー12",0,"guest12@gmail.com",0,0,now(),now()),
 (13,"admin","admin","インターノウス","管理者ユーザー","いんたーのうす","かんりしゃゆーざー",0,"admin@gmail.com",1,0,now(),now()),
-(14,"admin2","admin","インターノウス","管理者ユーザー2","いんたーのうす","かんりしゃゆーざー2",0,"admin2@gmail.com",1,0,now(),now()),
-(15,"admin3","admin","インターノウス","管理者ユーザー3","いんたーのうす","かんりしゃゆーざー3",0,"admin3@gmail.com",1,0,now(),now()),
-(16,"admin4","admin","インターノウス","管理者ユーザー4","いんたーのうす","かんりしゃゆーざー4",0,"admin4@gmail.com",1,0,now(),now()),
-(17,"admin5","admin","インターノウス","管理者ユーザー5","いんたーのうす","かんりしゃゆーざー5",0,"admin5@gmail.com",1,0,now(),now()),
-(18,"admin6","admin","インターノウス","管理者ユーザー6","いんたーのうす","かんりしゃゆーざー6",0,"admin6@gmail.com",1,0,now(),now()),
-(19,"admin7","admin","インターノウス","管理者ユーザー7","いんたーのうす","かんりしゃゆーざー7",0,"admin7@gmail.com",1,0,now(),now()),
-(20,"admin8","admin","インターノウス","管理者ユーザー8","いんたーのうす","かんりしゃゆーざー8",0,"admin8@gmail.com",1,0,now(),now()),
-(21,"admin9","admin","インターノウス","管理者ユーザー9","いんたーのうす","かんりしゃゆーざー9",0,"admin9@gmail.com",1,0,now(),now()),
-(22,"admin10","admin","インターノウス","管理者ユーザー10","いんたーのうす","かんりしゃゆーざー10",0,"admin10@gmail.com",1,0,now(),now()),
-(23,"admin11","admin","インターノウス","管理者ユーザー11","いんたーのうす","かんりしゃゆーざー11",0,"admin11@gmail.com",1,0,now(),now()),
-(24,"admin12","admin","インターノウス","管理者ユーザー12","いんたーのうす","かんりしゃゆーざー12",0,"admin12@gmail.com",1,0,now(),now());
+(14,"admin2","admin2","インターノウス","管理者ユーザー2","いんたーのうす","かんりしゃゆーざー2",0,"admin2@gmail.com",1,0,now(),now()),
+(15,"admin3","admin3","インターノウス","管理者ユーザー3","いんたーのうす","かんりしゃゆーざー3",0,"admin3@gmail.com",1,0,now(),now()),
+(16,"admin4","admin4","インターノウス","管理者ユーザー4","いんたーのうす","かんりしゃゆーざー4",0,"admin4@gmail.com",1,0,now(),now()),
+(17,"admin5","admin5","インターノウス","管理者ユーザー5","いんたーのうす","かんりしゃゆーざー5",0,"admin5@gmail.com",1,0,now(),now()),
+(18,"admin6","admin6","インターノウス","管理者ユーザー6","いんたーのうす","かんりしゃゆーざー6",0,"admin6@gmail.com",1,0,now(),now()),
+(19,"admin7","admin7","インターノウス","管理者ユーザー7","いんたーのうす","かんりしゃゆーざー7",0,"admin7@gmail.com",1,0,now(),now()),
+(20,"admin8","admin8","インターノウス","管理者ユーザー8","いんたーのうす","かんりしゃゆーざー8",0,"admin8@gmail.com",1,0,now(),now()),
+(21,"admin9","admin9","インターノウス","管理者ユーザー9","いんたーのうす","かんりしゃゆーざー9",0,"admin9@gmail.com",1,0,now(),now()),
+(22,"admin10","admin10","インターノウス","管理者ユーザー10","いんたーのうす","かんりしゃゆーざー10",0,"admin10@gmail.com",1,0,now(),now()),
+(23,"admin11","admin11","インターノウス","管理者ユーザー11","いんたーのうす","かんりしゃゆーざー11",0,"admin11@gmail.com",1,0,now(),now()),
+(24,"admin12","admin12","インターノウス","管理者ユーザー12","いんたーのうす","かんりしゃゆーざー12",0,"admin12@gmail.com",1,0,now(),now());
 
 create table product_info(
 id int primary key not  null auto_increment comment "ID",
@@ -56,18 +56,18 @@ product_id int unique not null comment "商品ID",
 product_name varchar(100) unique not null comment "商品名",
 product_name_kana varchar(100) unique not null comment "商品名かな",
 product_description varchar(255) unique not null comment "商品詳細",
-category_id int not null "カテゴリID",
+category_id int not null comment "カテゴリID",
 price int comment "価格",
 image_file_path varchar(100) comment "画像ファイルパス",
 image_file_name varchar(50) comment "画像ファイル名",
-release_date datetime not null comment "発売年月日",
-release_company varchar(50) comment "発売会社名"
+release_date datetime not null comment "発売年月",
+release_company varchar(50) comment "発売会社",
 status tinyint not null default 0 comment "ステータス",
 regist_date datetime not null comment "登録日",
 update_date datetime comment "更新日",
 foreign key(category_id) references m_category(category_id)
 )
-default charaset=ytf8
+default charset=utf8
 comment="商品情報テーブル"
 ;
 insert into product_info values
@@ -88,7 +88,7 @@ insert into product_info values
 (15,15,"おもちゃ5","おもちゃ5","おもちゃ5の商品詳細",4,500,"","","2010-10-20 10:00:00","internous",0,now(),now());
 
 create table cart_info(
-id int primary key not null auto_increment comment "ID"、
+id int primary key not null auto_increment comment "ID",
 user_id varchar(16) not null comment "ユーザーID",
 temp_user_id varchar(16) comment "仮ユーザーID",
 product_id int not null comment "商品ID",
@@ -97,18 +97,18 @@ price int not null comment "金額",
 regist_date datetime not null comment "登録日",
 update_date datetime comment "更新日"
 )
-default charaset=utf=8
+default charset=utf8
 comment="カート情報テーブル"
 ;
 insert into cart_info values
-(1,"guest",,1,1,100,now(),),
-(2,"guest2",,6,2,2000,now(),),
-(3,"guest2",,11,1,100,now(),);
+(1,"guest",null,1,1,100,now(),null),
+(2,"guest2",null,6,2,2000,now(),null),
+(3,"guest2",null,11,1,100,now(),null);
 
 create table purchase_history_info(
 id int primary key not null auto_increment comment "ID",
 user_id varchar(16) not null comment "ユーザーID",
-product_id int not null commenr\t "商品ID",
+product_id int not null comment "商品ID",
 product_count int not null comment "個数",
 price int not null comment "金額",
 destination_id int not null comment "宛先情報ID",
@@ -116,7 +116,7 @@ regist_date datetime not null comment "登録日",
 update_date datetime not null comment "更新日",
 foreign key(product_id) references product_info(product_id)
 )
-default charaset=utf8
+default charset=utf8
 comment="購入履歴情報テーブル"
 ;
 create table destination_info(
@@ -130,9 +130,9 @@ email varchar(32) not null comment "メールアドレス",
 tel_number varchar(13) not null comment "電話番号",
 user_address varchar(50) not null comment "住所",
 regist_date datetime not null comment "登録日",
-update_date date time comment "更新日"
+update_date datetime comment "更新日"
 )
-default charaset=utf8
+default charset=utf8
 comment="宛先情報テーブル"
 ;
 insert into destination_info values
@@ -146,10 +146,10 @@ category_description varchar(100) comment "カテゴリ詳細",
 insert_date datetime not null comment "登録日",
 update_date datetime comment "更新日"
 )
-default charaset=utf8
+default charset=utf8
 comment="カテゴリマスタテーブル"
 ;
-insert into m_category_info values
+insert into m_category values
 (1,1,"全てのカテゴリー","全てのカテゴリーが対象になります",now(),null),
 (2,2,"本","本に関するカテゴリーが対象になります",now(),null),
 (3,3,"家電","家電に関するカテゴリーが対象になります",now(),null),
