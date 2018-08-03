74fd6c8f4e4ab344998229cf256a6f6a923e886c
nanjo0420 <no_1akuson@yahoo.co.jp>
2018-08-03 14:15:44 +0900

    LoginActionとGoLoginActionをアップします。

---
+++ src/com/internousdev/tabicale/action/GoLoginAction.java
@@ -0,0 +1,53 @@
+package com.internousdev.tabicale.action;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.tabicale.dao.MCategoryDAO;
+import com.internousdev.tabicale.dto.MCategoryDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GoLoginAction extends ActionSupport implements SessionAware{
+
+	private String categoryId;
+	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
+	private Map<String, Object> session;
+
+	public String execute() {
+		if(!session.containsKey("mCategoryList")) {
+			MCategoryDAO mCategoryDao = new MCategoryDAO();
+			mCategoryDtoList = mCategoryDao.getMCategoryList();
+			session.put("mCategoryDtoList", mCategoryDtoList);
+		}
+		session.put("loginIdErrorMessageList", "");
+		session.put("passwordErrorMessageList", "");
+		return SUCCESS;
+	}
+
+	public String getCategoryId(){
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId){
+		this.categoryId = categoryId;
+	}
+
+	public List<MCategoryDTO> getmCategoryDtoList(){
+		return mCategoryDtoList;
+	}
+
+	public void setmCategoryDtoList(List<MCategoryDTO> mCategoryDtoList){
+		this.mCategoryDtoList = mCategoryDtoList;
+	}
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session){
+		this.session = session;
+	}
+}
