76ba0fd0baad62cdbe5ce5e49ee99db3fa17dfbf
ohashi0904 <ohashi904@yahoo.co.jp>
2018-08-06 13:43:37 +0900

    8/6 微修正

--- sql/tabicale.sql
+++ sql/tabicale.sql
@@ -1,8 +1,8 @@
 set names utf8;
 set foreign_key_checks=0;
 
-drop database if exists TabiCale;
-create database if not exists TabiCale;
+drop database if exists tabicale;
+create database if not exists tabicale;
 
 use tabicale;
 
