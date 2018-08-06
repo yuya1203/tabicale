94c01ac15ae3b7362327cff0e7e159efaa0ea5cb
ohashi0904 <ohashi904@yahoo.co.jp>
2018-08-06 16:49:31 +0900

    08/06 バグ修正

--- src/com/internousdev/tabicale/action/SettlementConfirmAction.java
+++ src/com/internousdev/tabicale/action/SettlementConfirmAction.java
@@ -63,7 +63,7 @@ public class SettlementConfirmAction extends ActionSupport implements SessionAwa
 		String[] releaseCompanyList = util.parseArrayList(releaseCompany);
 		String[] productCountList = util.parseArrayList(productCount);
 		String[] subtotalList = util.parseArrayList(subtotal);
-		for(int i = 0; i < productIdList.length; i++){
+		for(int i = 0; i < productIdList.length-1; i++){
 			PurchaseHistoryInfoDTO dto = new PurchaseHistoryInfoDTO();
 			dto.setUserId(String.valueOf(session.get("loginId")));
 			dto.setProductId(Integer.parseInt(String.valueOf(productIdList[i])));
