f64c281b0c71be2f5242786368d330431c4497e2
ohashi0904 <ohashi904@yahoo.co.jp>
2018-08-13 09:57:36 +0900

    08/13 微修正

--- src/com/internousdev/tabicale/dao/UserInfoDAO.java
+++ src/com/internousdev/tabicale/dao/UserInfoDAO.java
@@ -18,7 +18,7 @@ public class UserInfoDAO {
 		int count = 0;
 		String sql = "insert into user_info(user_id, password, family_name, first_name, family_name_kana,"
 				+ " first_name_kana, sex, email, status, logined, regist_date, update_date)"
-				+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), 0)";
+				+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), now())";
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			preparedStatement.setString(1, loginId);
