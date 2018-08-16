5472728c532a45d9c8c8967fcf06a515bf2625fc
oonishi-1123 <nero.dmc4se@gmail.com>
2018-08-16 10:42:44 +0900

    08/16

---
+++ src/com/internousdev/tabicale/dto/test/MCategoryDTOTest.java
@@ -0,0 +1,69 @@
+package com.internousdev.tabicale.dto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MCategoryDTOTest {
+
+	@Test
+	public void testGetId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetCategoryId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetCategoryId() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetCategoryName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetCategoryName() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetCategoryDescription() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetCategoryDescription() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetInsertDate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetInsertDate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetUpdateDate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testSetUpdateDate() {
+		fail("まだ実装されていません");
+	}
+
+}
